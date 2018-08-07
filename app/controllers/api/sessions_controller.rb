6b764b4e37b8e6f77366e75b9d24272ded876716
Aidan Gadberry <agadberr@gmail.com>
2018-08-07 16:15:58 -0700

    Add route methods for login, logout, and signup

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      email: params[:user][:email],
+      password: params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+    else
+      render json: ["Invalid username/password combination"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render json: ["Nobody signed in"], status: 404
+  end
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email, :fname, :lname, :user_url, :birthday, :sex, :password
+    )
+  end
+end
