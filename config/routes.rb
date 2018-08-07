1c087e4eb5248860ba8e32ac9fbb90d081248de5
Aidan Gadberry <agadberr@gmail.com>
2018-08-07 15:43:50 -0700

    Add api namespace and json formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
-  resource :session, only: [:create, :destroy]
+  namespace :api, defaults: {format: json} do
+    resources :users, only: [:new, :create]
+    resource :session, only: [:create, :destroy]
+  end
 end
