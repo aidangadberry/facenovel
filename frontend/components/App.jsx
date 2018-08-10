d78f944acb3e813a47f30f6e09cf80f5c177a562
Aidan Gadberry <agadberr@gmail.com>
2018-08-10 09:45:05 -0700

    Bootstrap current user to store

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LoginFormNavbar from './session/login_form_navbar';
 import LandingPage from './landing_page';
+import { AuthRoute } from 'react-router-dom';
 
 const App = () => {
   return (
--- frontend/facenovel.jsx
+++ frontend/facenovel.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
-  const root = document.getElementById('root');
+  let store;
 
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { currentUserId: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
+  const root = document.getElementById('root');
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  // window.logout = logout;
 
   ReactDOM.render(<Root store={store} />, root);
 });
