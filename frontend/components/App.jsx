fb1ae8ec611f62d0a0997b43d22e2874a1a8bed1
Aidan Gadberry <agadberr@gmail.com>
2018-08-10 09:51:36 -0700

    Move LoginNavbar into LandingPage

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import LoginFormNavbar from './session/login_form_navbar';
 import LandingPage from './landing_page';
 import { AuthRoute } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <LoginFormNavbar />
       <LandingPage />
     </div>
   )
--- frontend/components/landing_page.jsx
+++ frontend/components/landing_page.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SignupFormContainer from './session/signup_form_container';
+import LoginFormNavbar from './session/login_form_navbar';
 
 class LandingPage extends React.Component {
   render() {
     return (
-      <div className="landing-page-container">
-        <div className="recent-logins"></div>
-        <SignupFormContainer />
+      <div>
+        <LoginFormNavbar />
+        <div className="landing-page-container">
+          <div className="recent-logins"></div>
+          <SignupFormContainer />
+        </div>
       </div>
     )
   }
