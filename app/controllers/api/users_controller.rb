2c1cf580700f2fe54c76808c7507380be83ff757
Aidan Gadberry <agadberr@gmail.com>
2018-08-13 14:34:45 -0700

    Add route and action to search for a user by their profile's url

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,13 @@
 class Api::UsersController < ApplicationController
+  def show
+    @user = User.find_by(user_url: params[:user_url])
+    if @user
+      render "api/users/show";
+    else
+      render json: ["The user with that url does not exist"], status: 404
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+    get '/users/:user_url', to: 'users#show'
   end
 
   root 'static_pages#root'
