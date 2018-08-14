049391d5fcd4fd15893a6942b023560229adf823
Aidan Gadberry <agadberr@gmail.com>
2018-08-14 14:21:21 -0700

    Include assets in production build

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module Facenovel
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
   end
 end
