7fc0a739113bcf263e0684f8017be9e7d3e2ccbf
Aidan Gadberry <agadberr@gmail.com>
2018-08-15 16:50:44 -0700

    Add Friend model

---
+++ db/migrate/20180815234302_create_friends.rb
@@ -0,0 +1,10 @@
+class CreateFriends < ActiveRecord::Migration[5.1]
+  def change
+    create_table :friends do |t|
+      t.integer requesting_id, null: false
+      t.integer requested_id, null: false
+      t.boolean accepted, null: true, default: false
+      t.timestamps
+    end
+  end
+end
