239a8a414504ce0866af24760f690e3263d1431d
Aidan Gadberry <agadberr@gmail.com>
2018-08-15 15:41:18 -0700

    Update demo user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.find_by(email: "demo@demo.com").delete if (User.find_by(email: "demo@demo.com"))
 User.create(
   email: "demo@demo.com",
   password: "123123",
   fname: "Demo",
   lname: "Demothy",
-  user_url: "DemoUser"
-);
+  user_url: "DemoUser",
+  birthday: Date.new(1985, 7, 12)
+)
