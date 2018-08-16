1b02320de42a3e41b2b5afbb450852f9344efce3
Aidan Gadberry <agadberr@gmail.com>
2018-08-15 17:08:18 -0700

    Add unique constraints for friend pairs

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,5 @@
 class Friend < ApplicationRecord
+  validates :requesting_id, :requested_id, presence: true
+  validates :user_id, uniqueness: { scope: [:question_id] }
+  validates :accepted, inclusion: { in: [:true, :false] }
 end
