fc9bae4a1ae865ea100ea8603e3645c85b7429cc
Aidan Gadberry <agadberr@gmail.com>
2018-08-17 13:44:17 -0700

    Add responsive friend buttons to profile page

--- frontend/components/profiles/profile.jsx
+++ frontend/components/profiles/profile.jsx
@@ -4,16 +4,27 @@ import ProfileContent from './profile_content';
 
 class Profile extends React.Component {
   componentDidMount() {
-    this.props.fetchUserByUrl(this.props.userUrl);
+    this.props.fetchUserByUrl(this.props.userUrl).then(
+      res => this.props.fetchAssociatedRequest(this.props.currentUserId, res.user.id)
+    );
   }
 
   render() {
+    console.log("profile");
+    console.log(this.props);
     if (this.props.user === undefined) {
       return <div></div>
     } else {
       return (
         <div className="profile-container">
-          <ProfileCover user={this.props.user} />
+          <ProfileCover
+            user={this.props.user}
+            currentUserId={this.props.currentUserId}
+            friend={this.props.friend}
+            sendFriendRequest={this.props.sendFriendRequest}
+            approveFriendRequest={this.props.approveFriendRequest}
+            denyFriendRequest={this.props.denyFriendRequest}
+            />
           <ProfileContent user={this.props.user}/>
         </div>
       )
--- frontend/components/profiles/profile_container.js
+++ frontend/components/profiles/profile_container.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { fetchUserByUrl } from '../../actions/user_actions';
+import { getUserFromUrl, getRequest } from '../../reducers/selectors';
+import {
+  fetchAssociatedRequest,
+  sendFriendRequest,
+  approveFriendRequest,
+  denyFriendRequest
+} from '../../actions/friend_actions';
 import Profile from './profile';
-import { getUserFromUrl } from '../../reducers/selectors';
 
 const mapStateToProps = (state, ownProps) => ({
+  currentUserId: state.session.currentUserId,
   userUrl: ownProps.match.params.userUrl,
-  user: getUserFromUrl(state, ownProps.match.params.userUrl)
+  user: getUserFromUrl(state, ownProps.match.params.userUrl),
+  friend: getRequest(state, getUserFromUrl(state, ownProps.match.params.userUrl))
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchUserByUrl: url => dispatch(fetchUserByUrl(url))
+  fetchUserByUrl: url => dispatch(fetchUserByUrl(url)),
+  fetchAssociatedRequest: (currentUserId, userId) => dispatch(fetchAssociatedRequest(currentUserId, userId)),
+  sendFriendRequest: (currentUserId, userId) => dispatch(sendFriendRequest(currentUserId, userId)),
+  approveFriendRequest: friendId => dispatch(approveFriendRequest(friendId)),
+  denyFriendRequest: friendId => dispatch(denyFriendRequest(friendId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
