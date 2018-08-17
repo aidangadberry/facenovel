c1f9f3eac5a030c21c63fc3b7df8dc17b9d15826
Aidan Gadberry <agadberr@gmail.com>
2018-08-17 14:17:02 -0700

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.find_by(email: "demo@demo.com").delete if (User.find_by(email: "demo@demo.com"))
+User.destroy_all
+
 User.create(
-  email: "demo@demo.com",
-  password: "123123",
-  fname: "Demo",
-  lname: "Demothy",
-  user_url: "DemoUser",
+  email: 'demo@demo.com',
+  password: '123123',
+  fname: 'Demo',
+  lname: 'Demothy',
+  user_url: 'DemoUser',
   birthday: Date.new(1985, 7, 12)
 )
+User.create(
+  email: 'sjosefovic0@yahoo.com',
+  password: '123123',
+  fname: 'Sarah',
+  lname: 'Josefovic',
+  user_url: 'sjosefovic',
+  birthday: Date.new(1998, 5, 16),
+  sex: 'F'
+)
+User.create(
+  email: 'billston1@pacbell.net',
+  password: '123123',
+  fname: 'Blayne',
+  lname: 'Illston',
+  birthday: Date.new(1992, 7, 22),
+  sex: 'M'
+)
+User.create(
+  email: 'agadberr@gmail.com',
+  password: '123123',
+  fname: 'Aidan',
+  lname: 'Gadberry',
+  user_url: 'aidangadberry',
+  birthday: Date.new(1995, 10, 26),
+  sex: 'M'
+)
+User.create(
+  email: 'laurenj@gmail.com',
+  password: '123123',
+  fname: 'Lauren',
+  lname: 'Johnson',
+  birthday: Date.new(1994, 3, 8),
+  sex: 'F'
+)
+
+Friend.destroy_all
+Friend.create(requesting_id: User.third.id, requested_id: User.first.id, accepted: false)
+Friend.create(requesting_id: User.third.id, requested_id: User.fifth.id, accepted: true)
+Friend.create(requesting_id: User.first.id, requested_id: User.fourth.id, accepted: true)
+Friend.create(requesting_id: User.third.id, requested_id: User.fourth.id, accepted: false)
