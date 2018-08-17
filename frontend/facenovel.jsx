38b169311dac9003b47dc103e44d0b232e14cb9b
Aidan Gadberry <agadberr@gmail.com>
2018-08-17 13:53:44 -0700

    Preload all of a user's friends

--- frontend/facenovel.jsx
+++ frontend/facenovel.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import { logout } from './actions/session_actions';
+import { fetchAssociatedRequests } from './actions/friend_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -15,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
       session: { currentUserId: window.currentUser.id }
     };
     store = configureStore(preloadedState);
+    fetchAssociatedRequests(window.currentUser.id)(store.dispatch);
     delete window.currentUser;
   } else {
     store = configureStore();
@@ -24,6 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.logout = logout;
+  window.fetchAssociatedRequests = fetchAssociatedRequests;
 
   ReactDOM.render(<Root store={store} />, root);
 });
