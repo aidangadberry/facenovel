e0e31ebb58ccfc74982081e80a778b57957c1527
Aidan Gadberry <agadberr@gmail.com>
2018-08-17 13:45:56 -0700

    Add friend API endpoints

---
+++ app/controllers/api/friends_controller.rb
@@ -0,0 +1,60 @@
+class Api::FriendsController < ApplicationController
+  def index
+    @friends = Friend.where(requesting_id: params[:current_user_id]).to_a.concat(
+               Friend.where(requested_id: params[:current_user_id]).to_a
+    );
+
+    render "api/friends/index"
+  end
+
+  def show
+    @friend = Friend.where(
+      "(requesting_id = #{params[:current_user_id]} AND requested_id = #{params[:user_id]}) OR
+       (requesting_id = #{params[:user_id]} AND requested_id = #{params[:current_user_id]})"
+     ).first
+
+    render "api/friends/show"
+  end
+
+  def create
+    existingFriendship = Friend.friendship(friend_params)
+
+    if (existingFriendship)
+      @friend = existingFriendship
+      if (@friend.update(accepted: true))
+        render "api/friends/show"
+      else
+        render json: @friend.errors.full_messages, status: 422
+      end
+    else
+      @friend = Friend.new(friend_params)
+      if @friend.save
+        render "api/friends/show"
+      else
+        render json: @friend.errors.full_messages, status: 422
+      end
+    end
+  end
+
+  def update
+    @friend = Friend.find(params[:id])
+
+    if (@friend.update(accepted: true))
+      render "api/friends/show"
+    else
+      render json: @friend.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @friend = Friend.find(params[:id])
+
+    @friend.delete if @friend
+  end
+
+  private
+
+  def friend_params
+    params.require(:friend).permit(:requesting_id, :requested_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
     get '/users/:user_url', to: 'users#show'
+    resources :friends, only: [:create, :update, :destroy]
+    get '/users/:current_user_id/friends', to: 'friends#index'
+    get '/users/:current_user_id/friends/:user_id', to: 'friends#show'
   end
 
   root 'static_pages#root'
