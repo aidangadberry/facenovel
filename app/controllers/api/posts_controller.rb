1eee6110230bea4bd3e32304758cbe96ca9188c0
Aidan Gadberry <agadberr@gmail.com>
2018-09-22 16:37:07 -0700

    Add post show jbuilders

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,3 +1,26 @@
 class Api::PostsController < ApplicationController 
+  def index
+  end
 
+  def show
+    @post = Post.find(params[:id])
+    
+  end
+
+  def user_posts
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+  end
 end
---
+++ app/views/api/posts/_post.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! post, :id, :author_id, :body, :created_at
