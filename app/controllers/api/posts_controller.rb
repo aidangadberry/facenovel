a64bd0309971af940981828851e970aa0fe57c95
Aidan Gadberry <agadberr@gmail.com>
2018-09-22 16:44:09 -0700

    Add get posts by author id

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -4,10 +4,23 @@ class Api::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    
+
+    if @post
+      render "api/posts/show"
+    else
+      render json: ["A post with that id does not exist"], status: 404
+    end
   end
 
   def user_posts
+    @user = User.find(params[:user_id])
+
+    if @user
+      @posts = @user.authored_posts
+      render "api/posts/index"
+    else
+      render json: ["A user with that id does not exist"], status: 404
+    end
   end
 
   def create
