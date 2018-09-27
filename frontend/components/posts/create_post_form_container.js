3a00ebd9a4aa17aa82366e49eb57295df2077830
Aidan Gadberry <agadberr@gmail.com>
2018-09-27 11:16:55 -0700

    Add recipientId to post form based on current url location

--- frontend/components/posts/create_post_form_container.js
+++ frontend/components/posts/create_post_form_container.js
@@ -1,14 +1,31 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { createPost } from '../../actions/post_actions';
 import PostForm from './post_form';
 
-const mapStateToProps = state => ({
-  post: {body: '', authorId: state.session.currentUserId},
-  formType: 'Post'
-});
+const mapStateToProps = (state, ownProps) => {
+  const authorId = state.session.currentUserId;
+  const url = ownProps.match.params.userUrl;
+  let recipientId;
+  
+  if (url === undefined) {
+    recipientId = authorId;
+  } else {
+    recipientId = state.entities.userUrls[url];
+  }
+  
+  return ({
+    post: {
+      authorId,
+      recipientId,
+      body: '' 
+    },
+    formType: 'Post'
+  });
+};
 
 const mapDispatchToProps = dispatch => ({
   action: post => dispatch(createPost(post))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(PostForm);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PostForm));
