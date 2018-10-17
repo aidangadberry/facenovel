a70b2cb0e0d90c29efd1cc948cd89963fc8f49ed
Aidan Gadberry <agadberr@gmail.com>
2018-10-17 13:00:17 -0700

    Style comments

--- frontend/components/comments/comment_index_item.jsx
+++ frontend/components/comments/comment_index_item.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CommentIndexItem = ({ comment, author }) => (
-  <div>
-    {author.fname + ' ' + author.lname}
-    {comment.body}
+  <div className="comment-container">
+    <span className="comment-thumbnail" style={{ backgroundImage: `url(${author.profilePictureUrl})` }} />
+    <div className="comment-body">
+      <Link to={`/${author.userUrl}`} replace>
+        {author.fname} {author.lname}
+      </Link>
+      {' '}
+      {comment.body}
+    </div>
   </div>
 );
 
