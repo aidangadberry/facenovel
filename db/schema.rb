cc6593b260846f862543827a1825c30ce94ba269
Aidan Gadberry <agadberr@gmail.com>
2018-10-26 16:30:39 -0700

    Add friends table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_022837) do
+ActiveRecord::Schema.define(version: 2018_10_24_203137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
